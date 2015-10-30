f8334b3b84b8d01ac4c0a9cea3ccf9c2ab94ba16
fouric <fouric@protonmail.com>
2015-10-30 12:39:34 -0700

    Oh, duh - you access constants with +plusmuffs+; removed code that relied on an explicit constant accessor

--- src/termbox.lisp
+++ src/termbox.lisp
@@ -9,9 +9,6 @@
 
 (cffi:use-foreign-library termbox)
 
-; FIXME: the only reason why this is here is because we need tb-const to access constants
-(autowrap:c-include "/usr/include/termbox.h" :constant-accessor tb-const)
-
 (defun init ()
   (tb-init))
 
@@ -51,16 +48,11 @@
   (tb-set-cursor cx cy))
 
 
-(defun const (str)
-  (tb-const str))
-
 (defun test ()
   (init)
   (clear)
-  (change-cell 0 0 (char-code #\#) (tb-const "TB_WHITE") 0)
+  (change-cell 0 0 (char-code #\#) +tb-white+ 0)
   (present)
-  ;(plus-c:c-let ((event '(:struct (tb-event)) :free t))
-  ; (poll-event event))
   (with-alloc (event '(:struct (tb-event)))
     (poll-event event))
   (sleep 2)
